5a4230d4a9ba2382ebd9983f97abdd1cf02fb636
liu-du <duliujimmy@hotmail.com>
2018-10-12 20:49:35 +1100

    pass with 'brute force' algo, 100

--- week2/src/SeamCarver.java
+++ week2/src/SeamCarver.java
@@ -40,20 +40,21 @@ public class SeamCarver {
             right = p.getRGB(x + 1, y),
             x_dr = ((left >> 16) & 0xFF) - ((right >> 16) & 0xFF),
             x_dg = ((left >>  8) & 0xFF) - ((right >>  8) & 0xFF),
-            x_db = ((left >>  0) & 0xFF) - ((right >>  0) & 0xFF),
+            x_db = ( left        & 0xFF) - ( right        & 0xFF),
             verticalComponent = x_dr*x_dr + x_dg*x_dg + x_db*x_db;
 
         int up = p.getRGB(x, y - 1),
             down = p.getRGB(x, y + 1),
             y_dr = ((up >> 16) & 0xFF) - ((down >> 16) & 0xFF),
             y_dg = ((up >>  8) & 0xFF) - ((down >>  8) & 0xFF),
-            y_db = ((up >>  0) & 0xFF) - ((down >>  0) & 0xFF),
+            y_db = ( up        & 0xFF) - ( down        & 0xFF),
             horizontalComponent = y_dr*y_dr + y_dg*y_dg + y_db*y_db;
 
         return Math.sqrt(verticalComponent + horizontalComponent);
     }
 
     public int[] findVerticalSeam() {
+        if (width() == 1) return new int[height()];
         double[][] energy = new double[height()][width()];
         for (int j = 0; j < height(); j++) {
             for (int i = 0; i < width(); i++) {
@@ -65,6 +66,7 @@ public class SeamCarver {
     }
 
     public int[] findHorizontalSeam() {
+        if (height() == 1) return new int[width()];
         double[][] energy = new double[width()][height()];
         for (int i = 0; i < width(); i++) {
             for (int j = 0; j < height(); j++) {
@@ -99,7 +101,7 @@ public class SeamCarver {
                         distTo[j][i] = distTo[j - 1][i] + energy[j][i];
                         edgeTo[j][i] = i;
                     }
-                // right most
+                // rightmost
                 } else if (i == width - 1){
                     if (distTo[j - 1][i] < distTo[j - 1][i - 1]) {
                         distTo[j][i] = distTo[j - 1][i] + energy[j][i];
@@ -111,19 +113,19 @@ public class SeamCarver {
                 // middle
                 } else {
                     // upper right is shortest
-                    if (distTo[j - 1][i + 1] < distTo[j - 1][i] &&
-                            distTo[j - 1][i + 1] < distTo[j - 1][i - 1]) {
-                        distTo[j][i] = distTo[j - 1][i + 1] + energy[j][i];
-                        edgeTo[j][i] = i + 1;
+                    if (distTo[j - 1][i - 1] <= distTo[j - 1][i] &&
+                            distTo[j - 1][i - 1] < distTo[j - 1][i + 1]) {
+                        distTo[j][i] = distTo[j - 1][i - 1] + energy[j][i];
+                        edgeTo[j][i] = i - 1;
                     // up middle is shortest
-                    } else if (distTo[j - 1][i] < distTo[j - 1][i + 1] &&
-                            distTo[j - 1][i] < distTo[j - 1][i - 1]) {
+                    } else if (distTo[j - 1][i] <= distTo[j - 1][i + 1] &&
+                            distTo[j - 1][i] <= distTo[j - 1][i - 1]) {
                         distTo[j][i] = distTo[j - 1][i] + energy[j][i];
                         edgeTo[j][i] = i;
                     // upper left is shortest
                     } else {
-                        distTo[j][i] = distTo[j - 1][i - 1] + energy[j][i];
-                        edgeTo[j][i] = i - 1;
+                        distTo[j][i] = distTo[j - 1][i + 1] + energy[j][i];
+                        edgeTo[j][i] = i + 1;
                     }
                 }
             }
@@ -132,10 +134,10 @@ public class SeamCarver {
         double currentMin = Double.POSITIVE_INFINITY;
         int[] out = new int[height];
         out[height - 1] = 0;
-        for (int j = 0; j < width; j++) {
-            if (distTo[height - 1][j] < currentMin) {
-                currentMin = distTo[height - 1][j];
-                out[height - 1] = j;
+        for (int i = 0; i < width; i++) {
+            if (distTo[height - 1][i] < currentMin) {
+                currentMin = distTo[height - 1][i];
+                out[height - 1] = i;
             }
         }
 
@@ -146,73 +148,9 @@ public class SeamCarver {
         return out;
     }
 
-    // kind of a brute force implementation
-    private int[][] shortestPathTest() {
-
-        double[][] energy = new double[height()][width()];
-        for (int j = 0; j < height(); j++) {
-            for (int i = 0; i < width(); i++) {
-                // energy[0] represent energies at the top row
-                energy[j][i] = energy(i, j);
-            }
-        }
-        int width = energy[0].length;
-        int height = energy.length;
-
-        int[][] edgeTo = new int[height][width];
-        double[][] distTo = new double[height][width];
-
-        for (int i = 0; i < width; i++) {
-            distTo[0][i] = energy[0][i];
-        }
-
-        for (int j = 1; j < height; j++) {
-            for (int i = 0; i < width; i++) {
-                // leftmost
-                if (i == 0) {
-                    if (distTo[j - 1][i + 1] < distTo[j - 1][i]) {
-                        distTo[j][i] = distTo[j - 1][i + 1] + energy[j][i];
-                        edgeTo[j][i] = i + 1;
-                    } else {
-                        distTo[j][i] = distTo[j - 1][i] + energy[j][i];
-                        edgeTo[j][i] = i;
-                    }
-                    // right most
-                } else if (i == width - 1){
-                    if (distTo[j - 1][i] < distTo[j - 1][i - 1]) {
-                        distTo[j][i] = distTo[j - 1][i] + energy[j][i];
-                        edgeTo[j][i] = i;
-                    } else {
-                        distTo[j][i] = distTo[j - 1][i - 1] + energy[j][i];
-                        edgeTo[j][i] = i - 1;
-                    }
-                    // middle
-                } else {
-                    // upper right is shortest
-                    if (distTo[j - 1][i + 1] < distTo[j - 1][i] &&
-                            distTo[j - 1][i + 1] < distTo[j - 1][i - 1]) {
-                        distTo[j][i] = distTo[j - 1][i + 1] + energy[j][i];
-                        edgeTo[j][i] = i + 1;
-                        // up middle is shortest
-                    } else if (distTo[j - 1][i] < distTo[j - 1][i + 1] &&
-                            distTo[j - 1][i] < distTo[j - 1][i - 1]) {
-                        distTo[j][i] = distTo[j - 1][i] + energy[j][i];
-                        edgeTo[j][i] = i;
-                        // upper left is shortes
-                    } else {
-                        distTo[j][i] = distTo[j - 1][i - 1] + energy[j][i];
-                        edgeTo[j][i] = i - 1;
-                    }
-                }
-            }
-        }
-
-        return edgeTo;
-    }
-
-
     public void removeHorizontalSeam(int[] seam) {
         if (seam == null) throw new IllegalArgumentException("null arg");
+        if (seam.length != width()) throw new IllegalArgumentException("invalid seam");
         if (p.height() <= 1) throw new IllegalArgumentException("height <= 1");
         validateYIndex(seam[0]);
         for (int i = 1; i < seam.length; i++) {
@@ -235,6 +173,7 @@ public class SeamCarver {
 
     public void removeVerticalSeam(int[] seam) {
         if (seam == null) throw new IllegalArgumentException("null arg");
+        if (seam.length != height()) throw new IllegalArgumentException("invalid seam");
         if (p.width() <= 1) throw new IllegalArgumentException("width <= 1");
         validateXIndex(seam[0]);
         for (int i = 1; i < seam.length; i++) {
@@ -262,12 +201,12 @@ public class SeamCarver {
        StdOut.println(sc.width());
        StdOut.println(sc.height());
 
-       for (int j = 0; j < sc.height(); j++) {
-           for (int i = 0; i < sc.width(); i++) {
-               StdOut.print(String.format("%.2f ", sc.energy(i, j)));
-           }
-           StdOut.println();
-       }
+//       for (int j = 0; j < sc.height(); j++) {
+//           for (int i = 0; i < sc.width(); i++) {
+//               StdOut.print(String.format("%.2f ", sc.energy(i, j)));
+//           }
+//           StdOut.println();
+//       }
 
 //       StdOut.println(picture);
 //
@@ -277,36 +216,35 @@ public class SeamCarver {
 //       StdOut.println(sc.picture());
 
         StdOut.println();
-        StdOut.println();
 
-       int[][] test = sc.shortestPathTest();
+        double sum = 0.0;
+        int[] test1 = sc.findVerticalSeam();
 
-        for (int j = 0; j < sc.height(); j++) {
-            for (int i = 0; i < sc.width(); i++) {
-                StdOut.print(String.format("%s ", test[j][i]));
-            }
-            StdOut.println();
-        }
-
-//        StdOut.println();
-//        int[] test2 = sc.shortestPath();
-//
-//        for (int i = 0; i < test2.length; i++) {
-//            StdOut.print(String.format("%s ", test2[i]));
-//        }
-
-        StdOut.println();
-        int[] test3 = sc.findVerticalSeam();
-
-        for (int i = 0; i < test3.length; i++) {
-            StdOut.print(String.format("%s ", test3[i]));
+        for (int i = 0; i < test1.length; i++) {
+            StdOut.print(String.format("%s ", test1[i]));
+            sum += sc.energy(test1[i], i);
         }
 
         StdOut.println();
-        int[] test4 = sc.findHorizontalSeam();
+        int[] test2 = sc.findHorizontalSeam();
 
-        for (int i = 0; i < test4.length; i++) {
-            StdOut.print(String.format("%s ", test4[i]));
+        for (int i = 0; i < test2.length; i++) {
+            StdOut.print(String.format("%s ", test2[i]));
+        }
+
+        StdOut.println();
+        StdOut.println(sum);
+
+
+        double [][] en = {{1000.00, 1000.00, 1000.00, 1000.00, 1000.00},
+                          {1000.00,    3.46,    3.46,    3.32, 1000.00},
+                          {1000.00,    2.83,    4.12,    5.20, 1000.00},
+                          {1000.00,    4.00,    4.69,    3.46, 1000.00},
+                          {1000.00, 1000.00, 1000.00, 1000.00, 1000.00}};
+
+        int[] sp = sc.shortestPath(en);
+        for (int i = 0; i < sp.length; i++) {
+            StdOut.println(sp[i]);
         }
     }
 }
